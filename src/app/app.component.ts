12cebb1da9bd3e16d605b9eb9707684f913ed492
ran-konnecto <ran@konnecto.io>
2023-02-13 17:41:28 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+const texts = [
+  'This is a very very very big text This is a very very very big text This is a very very very big text This is a very very very big text This is a very very very big text This is a very very very big text This is a very very very bigtext This is a very very very big text This is a very very very big text sfesefsdfsdfdsf',
+  'hello',
+  'bla bla bla bla bla',
+];
+
+const numberOfTextsToShow = 1000;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ellipsis-tooltip-directive';
+  textData: string[] = [];
+
+  ngOnInit(): void {
+    for (let i = 0; i < numberOfTextsToShow; i++) {
+      this.textData.push(texts[Math.floor(Math.random() * texts.length)]);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { EllipsisTooltipDirective } from 'src/directives/ellipsis-tooltip.directive';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, EllipsisTooltipDirective],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
